224d12ef6729642e4de5a9f1f0546d1aad6ff1cc
Akil Krishnan <akilram@gmail.com>
2017-11-13 12:57:51 -0600

    Revise sum-of-multiples

--- sum-of-multiples/sum_of_multiples.cpp
+++ sum-of-multiples/sum_of_multiples.cpp
@@ -1,48 +1,32 @@
 #include "sum_of_multiples.h"
 
 
-int sum_of_multiples::to(const std::vector<int> integers, const int upper_limit)
+int sum_of_multiples::to(const std::set<int> integers, const int upper_limit)
 {
-	int sum_buffer = 0;
-
-	const auto& multiples = find_multiples(
-		std::set<int>(integers.cbegin(), integers.cend()), 
-		upper_limit
+	const auto& multiples = find_multiples(integers, upper_limit);
+	
+	const auto& sum = std::accumulate(
+		std::cbegin(multiples),
+		std::cend(multiples),
+		0
 	);
 
-	for (auto m : multiples)
-		sum_buffer += m;
-
-	return sum_buffer;
+	return sum;
 }
 
 std::vector<int> sum_of_multiples::find_multiples(
 	const std::set<int> integers,
 	const int upper_limit)
 {
-	std::set<int> multiples_buffer;
-	
-	for (const auto i : integers)
-	{
-		const auto& multiples = find_multiples(i, upper_limit);
-
-		for (auto m : multiples)
-			multiples_buffer.insert(m);
-	}
-
-	return 
-		std::vector<int>(multiples_buffer.cbegin(), multiples_buffer.cend());
-}
-
-std::vector<int> sum_of_multiples::find_multiples(
-	const int integer,
-	const int upper_limit)
-{
 	std::vector<int> multiples_buffer;
-
+	
 	for (int candidate = 1; candidate < upper_limit; candidate++)
 	{
-		const bool is_valid_multiple = candidate % integer == 0;
+		bool is_valid_multiple = false;
+		{
+			for (const auto& i : integers)
+				is_valid_multiple |= candidate % i == 0;
+		}
 		
 		if (is_valid_multiple)
 			multiples_buffer.push_back(candidate);
